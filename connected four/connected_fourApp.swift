f33076aeb6f0de953d9bd2c59f9e48b848c6ea99
T.Y.Kuan <tingyoungkuan@gmail.com>
2022-03-16 00:05:48 +0800

    Initial Commit

---
+++ connected four/connected_fourApp.swift
@@ -0,0 +1,17 @@
+//
+//  connected_fourApp.swift
+//  connected four
+//
+//  Created by tingyang on 2022/3/16.
+//
+
+import SwiftUI
+
+@main
+struct connected_fourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
